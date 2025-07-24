8f14221da2fcea3eebbf067d1b09c48c5d70802d
jiangjiyuanw <wjjy666@sjtu.edu.cn>
2025-07-24 16:19:58 -0400

    v5

--- lang/en.js
+++ lang/en.js
@@ -10,7 +10,7 @@ const enNavbarData = {
 // ---------- Home page data
 const enHomePageData = {
   name: "Jiangjiyuan Wang",
-  home_title: "Short Bio"
+  home_title: "Short Bio",
   home_content: `
     <div>
       <p>Hi! I am an undergraduate student at Shanghai Jiao Tong University.</p>
@@ -23,30 +23,10 @@ const enHomePageData = {
 // ---------- Publications page data
 const enPublicationsPageData = {
   type_one_title: "Papers",
-  type_one_items: [
-    // 示例填充
-    {
-//      title: "My First Paper Title",
-//      abstract: "This paper explores...",
-//      date: "2025",
-//      link: "http://example.com",
-//      github: "http://github.com/example",
-//      download: "",
-//      writers: ["Jiangjiyuan Wang", "Coauthor Name"],
-    },
-  ],
+  type_one_items: [],
 
   type_two_title: "Under Review",
-  type_two_items: [
-    {
-//      title: "Under Review Paper Title",
-//      abstract: "This paper is currently under review at...",
-//      date: "2025",
-//      link: "",
-//      github: "",
-//      writers: ["Jiangjiyuan Wang"],
-    },
-  ],
+  type_two_items: [],
 
   // 可以注释掉未使用的部分
   // type_three_title: "Book Chapters",
